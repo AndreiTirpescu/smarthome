132af29084aaab8b122244188a11cc3158f72e23
AndreiTirpescu <tirpescu.andrei@yahoo.com>
2024-02-20 11:43:15 +0200

    [INFRASTRUCTURE] prepare infra for domain events
    [INFRASTRUCTURE] Add RabbitMQ to docker compose and start script files

--- infrastructure/start.sh
+++ infrastructure/start.sh
@@ -2,4 +2,4 @@ echo "Stop current running to avoid port conflict"
 docker stop $(docker ps -q)
 
 echo "Starting smarthome dev containers"
-docker start smarthome-db  
+docker start smarthome-db smarthome-rabbitmq
